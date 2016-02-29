8b6f9673a59fbe12b71371ccfe60136cf1007256
goalong <goforalong@gmail.com>
2016-02-29 20:37:34 +0800

    delete .pyc

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,12 @@
+# encoding: utf-8
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email
 
 
 class LoginForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField(u'邮箱', validators=[Required(), Length(1, 64),
                                              Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+    password = PasswordField(u'密码', validators=[Required()])
+    remember_me = BooleanField(u'记住我')
+    submit = SubmitField(u'登录')
